ad111ce4b9bcd5347e0225b6f0cbeaf07359bf39
delyee <delyee0@protonmail.com>
2022-05-28 15:21:18 +0300

    first version

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+ABUSEIPDB_API_KEY = "111111fe15ed381ed11111169a96ae4136fa111111c968c1e1aa8fc6a3d527111111d79cf01c74a1"
+ABUSEIPDB_API_PATH = "https://api.abuseipdb.com/api/v2/blacklist"
+ABUSEIPDB_OPTIONS = {
+    "confidenceMinimum": "70",
+    "limit": "10000"  # Standard => 10000, Basic Subscription => 100000, Premium Subscription => 500000
+}
 
+NGINX_FORMAT = "deny {ip};  # blacklist_generator_abuseipdb\n"
+HOSTS_DENY_FORMAT = "ALL: {ip}  # blacklist_generator_abuseipdb\n"
--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from config import *
+from requests import get as requests_get
+from optparse import OptionParser
 from loguru import logger
+from config import *
 
-logger.add("ChangeMe_{time}.log.log", rotation="1 GB", compression="gz")
-# logger.info("Starting...")
-# logger.add(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>")
+
+parser = OptionParser()
+parser.add_option("-m", "--mode", dest="mode", help="Mode: hosts.deny or nginx",
+                  choices=["nginx", "hosts.deny"], default="nginx")
+parser.add_option("-p", "--path", dest="path", help="Dst filepath: \"/etc/hosts.deny\" or \"/etc/nginx/conf.d/blacklist.conf\"",
+                  choices=["/etc/nginx/conf.d/blacklist.conf", "/etc/hosts.deny"], default="/etc/nginx/conf.d/blacklist.conf")
+parser.add_option("-k", "--key", dest="api_key", help="API key for abuseipdb", default=None)
+(options, args) = parser.parse_args()
+
+logger.add("blacklist_generator_abuseipdb.log", format="{time} {message}", rotation="1 GB", compression="gz")
 
 # handler = logging.handlers.SysLogHandler(address=('localhost', 514))
 # logger.add(handler)
 
-# logger.level("SNAKY", no=38, color="<yellow>", icon="🐍")
-# logger.log("SNAKY", "Here we go!")
+if not options.api_key:
+    options.api_key = ABUSEIPDB_API_KEY
+
+text = requests_get(ABUSEIPDB_API_PATH, headers={"Accept": "text/plain", "Key": options.api_key}, params=ABUSEIPDB_OPTIONS).text
+if "AbuseIPDB APIv2 Server" in text:
+    logger.critical("your API key is invalid")
+    exit()
+
+
+ips = text.split("\n")
+
+with open(options.path, "a") as f:
+    if options.mode == "nginx":
+        for ip in ips:
+            f.write(NGINX_FORMAT.format(ip=ip))
+
+    elif options.mode == "hosts.deny":
+        for ip in ips:
+            f.write(HOSTS_DENY_FORMAT.format(ip=ip))
+
+
+logger.info("success add {} ips".format(len(ips)))
